7d2a72754faaaef226ce65d153c0635c9a0a4cc4
damlys <damian.lysiak@gmail.com>
2022-08-14 11:17:14 +0200

    linter

--- cmd/goapp/main.go
+++ cmd/goapp/main.go
@@ -9,13 +9,17 @@ func main() {
 	http.HandleFunc("/-/ready", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Add("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusOK)
-		res.Write([]byte("Ready."))
+		if _, err := res.Write([]byte("Ready.")); err != nil {
+			log.Printf("ready write error: %s\n", err)
+		}
 	})
 
 	http.HandleFunc("/-/healthy", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Add("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusOK)
-		res.Write([]byte("Healthy."))
+		if _, err := res.Write([]byte("Healthy.")); err != nil {
+			log.Printf("healthy write error: %s\n", err)
+		}
 	})
 
 	http.HandleFunc("/hello", Hello)
